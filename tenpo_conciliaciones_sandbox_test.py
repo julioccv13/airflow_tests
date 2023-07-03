cea4940a9182c4f4889bb7e5becc50cc75a0577d
Julio Cristancho <j.cristancho.externo@tenpo.cl>
2023-07-03 10:27:37 -0500

    fix dag

--- tenpo_conciliaciones_sandbox_test.py
+++ tenpo_conciliaciones_sandbox_test.py
@@ -1,13 +1,14 @@
 from airflow.models import DAG
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.providers.google.cloud.operators.dataproc import DataprocInstantiateWorkflowTemplateOperator
 from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
 from airflow.providers.google.cloud.sensors.gcs import GCSObjectsWithPrefixExistenceSensor
 from airflow.providers.google.cloud.hooks.gcs import GCSHook
 from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 from airflow.providers.google.common.hooks.base_google import GoogleBaseHook
+from airflow.utils.trigger_rule import TriggerRule
 from airflow.utils.dates import days_ago
 from airflow.utils.state import State
 from google.cloud import bigquery
@@ -20,15 +21,18 @@ default_args = {
     "start_date"      : days_ago( 1 ),
     "retries"         : 1,
     "retry_delay"     : datetime.timedelta( minutes= 10 ),
-    "email_on_failure": False,
+    "email_on_failure": True,
     "email_on_retry": False,
 }
 
 # DAG Variables used
 PROJECT_NAME = Variable.get('project1')
-SOURCE_BUCKET = 'mark-vii-conciliacion'
+SOURCE_PROJECT = Variable.get('conciliacion_uat')
+SOURCE_BUCKET = 'conciliacion-uat-test'
 TARGET_BUCKET = 'mark-vii-conciliacion'
+DATA_BUCKET = 'mark-vii-conciliacion'
 PREFIX = 'sql'
+GCS_CONN_ID = 'conciliacion_uat'
 
 # Reads sql files from GCS bucket
 def read_gcs_sql(query):
@@ -38,7 +42,7 @@ def read_gcs_sql(query):
     else:
         object_name = f'{query}'
     resp_byte = hook.download_as_byte_array(
-    bucket_name = SOURCE_BUCKET,
+    bucket_name = DATA_BUCKET,
     object_name = object_name,
             )
 
@@ -74,6 +78,7 @@ with DAG(
         task_id= "opd_sensor",
         bucket=SOURCE_BUCKET,
         prefix='data/opd/PLJ61110.FINT0003',
+        google_cloud_conn_id=GCS_CONN_ID,
         poke_interval=60*10 ,
         mode='reschedule',
         timeout=60*30,
@@ -84,6 +89,7 @@ with DAG(
         task_id= "ipm_sensor",
         bucket=SOURCE_BUCKET,
         prefix='data/ipm/MCI.AR.T112.M.E0073610.D',
+        google_cloud_conn_id=GCS_CONN_ID,
         poke_interval=60*10 ,
         mode='reschedule',
         timeout=60*30,
@@ -94,6 +100,7 @@ with DAG(
         task_id= "anulation_sensor",
         bucket=SOURCE_BUCKET,
         prefix='data/anulation/PLJ00032.TRXS.ANULADAS',
+        google_cloud_conn_id=GCS_CONN_ID,
         poke_interval=60*10 ,
         mode='reschedule',
         timeout=60*30,
@@ -104,39 +111,92 @@ with DAG(
         task_id= "incident_sensor",
         bucket=SOURCE_BUCKET,
         prefix='data/incident/PLJ62100-CONS-INC-PEND-TENPO',
+        google_cloud_conn_id=GCS_CONN_ID,
         poke_interval=60*10 ,
         mode='reschedule',
         timeout=60*30,
         soft_fail=True,
         )
     
-# Bash commands to start a dataproc workflow template to process each file type
-    dataproc_ipm = BashOperator(
-        task_id="start_dataproc_ipm",
-        bash_command="gcloud dataproc workflow-templates instantiate template_process_file --region=us-central1 --parameters=CLUSTER=tenpo-ipm-prod,NUMWORKERS=16,JOBFILE=gs://mark-vii-conciliacion/artifacts/dataproc/pyspark_data_process.py,FILES_OPERATORS=gs://mark-vii-conciliacion/artifacts/dataproc/operators/*,INPUT=gs://mark-vii-conciliacion/data/ipm/MCI.AR.T112.M.E0073610.D*,TYPE_FILE=ipm,OUTPUT=tenpo-datalake-sandbox.tenpo_conciliacion_staging_dev.ipm,MODE_DEPLOY=prod ",
-        )
-    
-    dataproc_opd = BashOperator(
-        task_id="start_dataproc_opd",
-        bash_command="gcloud dataproc workflow-templates instantiate template_process_file --region=us-central1 --parameters=CLUSTER=tenpo-opd-prod,NUMWORKERS=16,JOBFILE=gs://mark-vii-conciliacion/artifacts/dataproc/pyspark_data_process.py,FILES_OPERATORS=gs://mark-vii-conciliacion/artifacts/dataproc/operators/*,INPUT=gs://mark-vii-conciliacion/data/opd/PLJ61110.FINT0003*,TYPE_FILE=opd,OUTPUT=tenpo-datalake-sandbox.tenpo_conciliacion_staging_dev,MODE_DEPLOY=prod",
-        )    
-    
-    dataproc_anulation = BashOperator(
-        task_id="start_dataproc_anulation",
-        bash_command="gcloud dataproc workflow-templates instantiate template_process_file --region=us-central1 --parameters=CLUSTER=tenpo-opd-anulation-prod,NUMWORKERS=16,JOBFILE=gs://mark-vii-conciliacion/artifacts/dataproc/pyspark_data_process.py,FILES_OPERATORS=gs://mark-vii-conciliacion/artifacts/dataproc/operators/*,INPUT=gs://mark-vii-conciliacion/data/anulation/PLJ00032.TRXS.ANULADAS*,TYPE_FILE=anulation,OUTPUT=tenpo-datalake-sandbox.tenpo_conciliacion_staging_dev.opd_anulation,MODE_DEPLOY=prod&",
+# Instantiate a dataproc workflow template for each type of file to process 
+    dataproc_ipm = DataprocInstantiateWorkflowTemplateOperator(
+        task_id="dataproc_ipm",
+        project_id=PROJECT_NAME,
+        region='us-central1',
+        template_id='template_process_file',
+        gcp_conn_id='conciliacion_uat',
+        parameters={
+            'CLUSTER':'tenpo-ipm-prod',
+            'NUMWORKERS':'4',
+            'JOBFILE':'gs://mark-vii-conciliacion/artifacts/dataproc/pyspark_data_process.py',
+            'FILES_OPERATORS':'gs://mark-vii-conciliacion/artifacts/dataproc/operators/*',
+            'INPUT':'gs://conciliacion-uat-test@tenpo-mark-vii/data/ipm/MCI.AR.T112.M.E0073610.D*',
+            'TYPE_FILE':'ipm',
+            'OUTPUT':'tenpo-datalake-sandbox.tenpo_conciliacion_staging_dev.ipm',
+            'MODE_DEPLOY':'prod'
+        },
         )
 
-    dataproc_incident = BashOperator(
-        task_id="start_dataproc_incident",
-        bash_command="gcloud dataproc workflow-templates instantiate template_process_file --region=us-central1 --parameters=CLUSTER=tenpo-opd-incident-prod,NUMWORKERS=16,JOBFILE=gs://mark-vii-conciliacion/artifacts/dataproc/pyspark_data_process.py,FILES_OPERATORS=gs://mark-vii-conciliacion/artifacts/dataproc/operators/*,INPUT=gs://mark-vii-conciliacion/data/incident/PLJ62100-CONS-INC-PEND-TENPO*,TYPE_FILE=incident,OUTPUT=tenpo-datalake-sandbox.tenpo_conciliacion_staging_dev.opd_incident,MODE_DEPLOY=prod&",
-        )        
+    dataproc_opd = DataprocInstantiateWorkflowTemplateOperator(
+        task_id='dataproc_opd',
+        project_id=PROJECT_NAME,
+        region='us-central1',
+        template_id='template_process_file',
+        gcp_conn_id='conciliacion_uat',
+        parameters={
+            'CLUSTER':'tenpo-opd-prod',
+            'NUMWORKERS':'8',
+            'JOBFILE':'gs://mark-vii-conciliacion/artifacts/dataproc/pyspark_data_process.py',
+            'FILES_OPERATORS':'gs://mark-vii-conciliacion/artifacts/dataproc/operators/*',
+            'INPUT':'gs://conciliacion-uat-test@tenpo-mark-vii/data/opd/PLJ61110.FINT0003*',
+            'TYPE_FILE':'opd',
+            'OUTPUT':'tenpo-datalake-sandbox.tenpo_conciliacion_staging_dev',
+            'MODE_DEPLOY':'prod'
+        },
+        )
+
+    dataproc_anulation = DataprocInstantiateWorkflowTemplateOperator(
+        task_id='dataproc_anulation',
+        project_id=PROJECT_NAME,
+        region='us-central1',
+        template_id='template_process_file',
+        gcp_conn_id='conciliacion_uat',
+        parameters={
+            'CLUSTER':'tenpo-opd-anulation-prod',
+            'NUMWORKERS':'4',
+            'JOBFILE':'gs://mark-vii-conciliacion/artifacts/dataproc/pyspark_data_process.py',
+            'FILES_OPERATORS':'gs://mark-vii-conciliacion/artifacts/dataproc/operators/*',
+            'INPUT' : 'gs://conciliacion-uat-test@tenpo-mark-vii/PLJ00032.TRXS.ANULADAS*',
+            'TYPE_FILE':'anulation',
+            'OUTPUT':'tenpo-datalake-sandbox.tenpo_conciliacion_staging_dev.opd_anulation',
+            'MODE_DEPLOY':'prod'
+        },
+        )
+
+    dataproc_incident = DataprocInstantiateWorkflowTemplateOperator(
+        task_id='dataproc_incident',
+        project_id=PROJECT_NAME,
+        region='us-central1',
+        template_id='template_process_file',
+        gcp_conn_id='conciliacion_uat',
+        parameters={
+            'CLUSTER':'tenpo-opd-incident-prod',
+            'NUMWORKERS':'4',
+            'JOBFILE':'gs://mark-vii-conciliacion/artifacts/dataproc/pyspark_data_process.py',
+            'FILES_OPERATORS':'gs://mark-vii-conciliacion/artifacts/dataproc/operators/*',
+            'INPUT':'gs://conciliacion-uat-test@tenpo-mark-vii/data/incident/PLJ62100-CONS-INC-PEND-TENPO*',
+            'TYPE_FILE':'incident',
+            'OUTPUT':'tenpo-datalake-sandbox.tenpo_conciliacion_staging_dev.opd_incident',
+            'MODE_DEPLOY':'prod'
+        },
+        ) 
 
 # Read a sql file for each type of file pass the tables from staging to gold    
     read_ipm_gold = PythonOperator(
         task_id='read_ipm_gold',
         provide_context=True,
         python_callable=read_gcs_sql,
-        op_kwargs={
+                op_kwargs={
         "query": "ipm_staging_to_gold.sql"
         }
         )
@@ -210,6 +270,7 @@ with DAG(
         task_id='read_match',
         provide_context=True,
         python_callable=read_gcs_sql,
+        trigger_rule=TriggerRule.ALL_DONE,
         op_kwargs={
         "query": "match.sql"
         }
@@ -233,7 +294,7 @@ with DAG(
         destination_bucket=TARGET_BUCKET,
         destination_object='conciliacion_backup/ipm/MCI.AR.T112.M.E0073610.D',
         move_object=True
-    )
+        )
 
     move_files_opd = GCSToGCSOperator(
         task_id='move_files_opd',
@@ -242,7 +303,7 @@ with DAG(
         destination_bucket=TARGET_BUCKET,
         destination_object='conciliacion_backup/opd/PLJ61110.FINT0003',
         move_object=True
-    )
+        )
 
     move_files_anulation = GCSToGCSOperator(
         task_id='move_files_anulation',
@@ -251,7 +312,7 @@ with DAG(
         destination_bucket=TARGET_BUCKET,
         destination_object='conciliacion_backup/anulation/PLJ00032.TRXS.ANULADAS',
         move_object=True
-    )
+        )
 
     move_files_incident = GCSToGCSOperator(
         task_id='move_files_incident',
@@ -260,7 +321,7 @@ with DAG(
         destination_bucket=TARGET_BUCKET,
         destination_object='conciliacion_backup/incident/PLJ62100-CONS-INC-PEND-TENPO',
         move_object=True
-    )
+        )
 
 # Dummy tasks        
     start_task = DummyOperator( task_id = 'start')
@@ -278,6 +339,3 @@ execute_match >> [move_files_ipm, move_files_opd, move_files_anulation, move_fil
 
 # Failure callback
 dag.on_failure_callback = task_failure
-
-
-
